642b0fc49aa04b2b419d1d13ddc130259c7500ca
Doug-13 <douglasazevedomello2@gmail.com>
2023-09-03 18:06:49 -0300

    atualizado dados

--- pages/3_Dados por Campeonato.py
+++ pages/3_Dados por Campeonato.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import pandas as pd
+# import matplotlib.pyplot as plt
+# import pandas as pd
 
 # doug-mello@projfutebol.iam.gserviceaccount.com
 # Importar funções
